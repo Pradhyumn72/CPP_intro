7cc477e3615ab58ae7f6ae4dada09e2a70bfe2fb
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-03-20 19:23:52 +0530

    first commit

---
+++ square.cpp
@@ -0,0 +1,43 @@
+// write a program to generate square of any number 
+// write a program to generate cube of any number 
+// write a program to display simple intrest 
+// WAP to print the area of circle 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a;
+    // int b;
+    //  int c;
+    //   int d;
+    //   int P;
+    //     int R ;
+    //           int T;
+    //               int Amount;
+
+    // cout<<"Enter value of number you want to get sqaured : ";
+    // cin>>a;
+    // b=a*a;
+    // cout<<"square of a is "<<b;
+    // cout<<"\n";
+
+    // cout<<"Enter value of number you want to get cubed :";
+    // cin>>c;
+   
+    // d=c*c*c;
+    // cout<<d;
+    // cout<<"cube is  of a is \n"<<d;
+    
+    
+    // cout<<"enter amount : \n";
+    // cin>>P;
+    // cout<<"Enter rate : \n";
+    // cin>>R;
+    // cout<<"Enter duartion (in months) \n";
+    // cin>>T;
+    // Amount = (P*R*T)/100;
+    // cout<<"Simple intrest is : \n"<<Amount;
+
+
+}
