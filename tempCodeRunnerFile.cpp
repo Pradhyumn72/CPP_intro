86b570a93f58977f9849f1d16bc5e0bdeb418b3e
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-03-24 19:37:30 +0530

    updated

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+int d= -9 ;
+//  int e =10;
+//  int f = d && e ;
+//  cout<<f; //0
--- types_of_operators.cpp
+++ types_of_operators.cpp
@@ -7,16 +7,17 @@
 using namespace std;
  int main()
  {
-//  int a=8 ;
-//  int b =20;
-//  int c = a && b ;
+ int a ;
+ int b ;
+ int c;
+//  int c = a &&  ;
 //  cout<<c; //1
 //  output 1 tells us that the c is true as it compares both the values ..if both are non zero values output will be 1..
 // if any one input is zero ,output will be 0 (false)
-int d= -9 ;
- int e =10;
- int f = d && e ;
- cout<<f; //0
+// int d= -9 ;
+//  int e =10;
+//  int f = d && e ;
+//  cout<<f; //0
 
  //OR(||)
 //  int x =10;
@@ -26,6 +27,20 @@ int d= -9 ;
 
 // Realtional Operators
 // >,<, >=,<= ,!=
+
+// ternary operators
+//WAP for entering no is odd or even
+// cout<<"enter the number: \n";
+// cin>>a;
+// (a%2==0) ? cout<<"odd number ":cout<<"even number";
+// cout<<b;
+
+// WAP which returns absolute value
+cout<<"enter a value ";
+
+cin>>a;
+(a>=0) ?cout<<a:cout<<-a;
+
 
 
  }
