e628313e4560900f8cccdef4fa09deaf445c304a
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-03-28 19:29:34 +0530

    updated

---
+++ if_else_intro.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //age > eligible 
+
+    int a ;
+    int b;
+    cout<<"enter value of a :";
+    cin>>a;
+    cout<<"enter value of b :";
+    cin>>b;
+    if(a>b){
+        cout<<"larger value is a "<<a;
+    }
+    else{
+        cout<<"larger value is b "<<b;
+
+    }
+    // cout<<"enter value of a: ";
+    // cin>>a;
+    // if (a%2==0){
+    //     cout<<"even";
+    // }
+    // else{
+    //     cout<<"odd";
+    // }
+    
+}
