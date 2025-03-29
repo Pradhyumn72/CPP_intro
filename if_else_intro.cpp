71b39c2aef1987d266fa2b642b2cc68a876d4d06
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-03-29 19:15:59 +0530

    updated

--- if_else_intro.cpp
+++ if_else_intro.cpp
@@ -3,19 +3,19 @@ using namespace std;
 int main(){
     //age > eligible 
 
-    int a ;
-    int b;
-    cout<<"enter value of a :";
-    cin>>a;
-    cout<<"enter value of b :";
-    cin>>b;
-    if(a>b){
-        cout<<"larger value is a "<<a;
-    }
-    else{
-        cout<<"larger value is b "<<b;
+    // int a ;
+    // int b;
+    // cout<<"enter value of a :";
+    // cin>>a;
+    // cout<<"enter value of b :";
+    // cin>>b;
+    // if(a>b){
+    //     cout<<"larger value is a "<<a;
+    // }
+    // else{
+    //     cout<<"larger value is b "<<b;
 
-    }
+    // }
     // cout<<"enter value of a: ";
     // cin>>a;
     // if (a%2==0){
@@ -24,5 +24,26 @@ int main(){
     // else{
     //     cout<<"odd";
     // }
+
+
+// else ladder
+
+    // int price;
+    // cout<<"enter the price of the product ";
+    // cin>>price;
+    // if(price<1000){
+    //     cout<<"You will get 10% discount and the price of the prouct after discount will be "<<price-0.1*price;
+    // }
+    // else if(price >=1000 && price<=2000){
+    //     cout<<"You will get 15% discount and the price of the prouct after discount will be "<<price-0.15*price;
+    // }
+    // else if (price>2000){
+    //     cout<<"You will get 20% discount and the price of the prouct after discount will be "<<price-0.2*price;
+    // }
     
+
+// nested if
+
+
+
 }
