acf36bdd292ab8342aaea58469d8076220b7d22a
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-03-31 19:14:34 +0530

    updated

--- if_else_intro.cpp
+++ if_else_intro.cpp
@@ -44,25 +44,56 @@ int main(){
 
 // nested if
 
-int marks;
-cout<<"enter marks ";
-cin>>marks;
-if (marks<33){
-    cout<<"fail";
+// int marks;
+// cout<<"enter marks ";
+// cin>>marks;
+// if (marks<33){
+//     cout<<"fail";
+// }
+// else{
+//     cout<<"pass by ";
+//     if(marks>33 && marks<=45){
+//         cout<<"3rd division";
+//     }
+//     if(marks>45 && marks<=60){
+//         cout<<"2nd divison";
+//     }
+//     if(marks>60 && marks<=100){
+//         cout<<"1st division";
+//     }
+// }
+
+// WAP to enter password ...2025 should be there
+
+// int password;
+// cout<<"Enter your password";
+// cin>>password;
+
+// if (password == 2025){
+//     cout<<"Welcome";
+// }
+// else{
+//     cout<<"invalid password";
+// }
+
+// WAP to match aadhar then enter ur age ...else invalid aadhar....if age entered>18 u can drive..
+
+int adhr,age;
+
+cout<<"enter ur aadhar number";
+cin>>adhr;
+if(adhr==1234){
+    cout<<"enter your age";
+    cin>>age;
+    if(age>17){
+        cout<<"u r eligible to drive";
+    }
+    else{
+        cout<<" u r not eligible to drive";
+    }
 }
 else{
-    cout<<"pass by ";
-    if(marks>33 && marks<=45){
-        cout<<"3rd division";
-    }
-    if(marks>45 && marks<=60){
-        cout<<"2nd divison";
-    }
-    if(marks>60 && marks<=100){
-        cout<<"1st division";
-    }
+    cout<<"invalid aadhar";
 }
-
-
 
 }
