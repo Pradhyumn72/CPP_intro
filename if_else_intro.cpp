9d978852e920e2987bf10fdf19de6a47b7d533f1
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-04-01 19:06:32 +0530

    updated

--- if_else_intro.cpp
+++ if_else_intro.cpp
@@ -78,22 +78,76 @@ int main(){
 
 // WAP to match aadhar then enter ur age ...else invalid aadhar....if age entered>18 u can drive..
 
-int adhr,age;
+// int adhr,age;
 
-cout<<"enter ur aadhar number";
-cin>>adhr;
-if(adhr==1234){
-    cout<<"enter your age";
-    cin>>age;
-    if(age>17){
-        cout<<"u r eligible to drive";
-    }
-    else{
-        cout<<" u r not eligible to drive";
-    }
+// cout<<"enter ur aadhar number";
+// cin>>adhr;
+// if(adhr==1234){
+//     cout<<"enter your age";
+//     cin>>age;
+//     if(age>17){
+//         cout<<"u r eligible to drive";
+//     }
+//     else{
+//         cout<<" u r not eligible to drive";
+//     }
+// }
+// else{
+//     cout<<"invalid aadhar";
+// }
+
+
+//WAP...enter 3 numbers .....return largest number 
+
+// int a ,b,c;
+// cout<<"enter val of a :";
+// cin>>a;
+// cout<<"enter val of b:";
+// cin>>b;
+// cout<<"enter val of c :";
+// cin>>c;
+// if (a>b && a>c){
+//     cout<<" a is larger";
+// }
+// else if (b>a && b>c){
+// cout<<"b is larger";
+// }
+// else{
+//     cout<<"c is larger";
+// }
+
+//WAP to generate the bill of electricity by taking  
+// following constraints
+
+int units;
+cout<<"Enter the no of units consumed";
+cin>>units;
+if(units>=0 && units<=50){
+    cout<<"your bill is Rs. "<<units*2 + 0.2*units*2;
+}
+else if (units>50 && units<=100)
+{
+    cout<<"your bill is Rs. "<<units*3 + 0.2*units*3;
+}
+
+else if (units>100 && units<=500)
+{
+    cout<<"your bill is Rs. "<<units*5 + 0.2*units*5;
+}
+else if (units>500 && units<=1000)
+{
+    cout<<"your bill is Rs. "<<units*7 + 0.2*units*7;
+}
+else if (units>=1000)
+{
+    cout<<"your bill is Rs. "<<units*10 + 0.2*units*10;
 }
 else{
-    cout<<"invalid aadhar";
+    cout<<"Invalid Reading";
 }
+
+
+
+
 
 }
