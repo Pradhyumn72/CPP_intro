538fb9bdb31692f92b4b2cedffc30d2b8a752802
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-04-03 19:12:15 +0530

    updated

--- Switch_case.cpp
+++ Switch_case.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+int main() {
+    int d;
+    char f;
+
+    cout << "Enter A for area of circle, C for circumference, S for square: ";
+    cin >> f;
+
+    switch (f) {
+        case 'A':
+        case 'a': {
+            cout << "Enter radius of the circle: ";
+            cin >> d;
+            cout << "Area of circle is " << 3.14 * d * d << endl;
+            break;
+        }
+        case 'C':
+        case 'c': {
+            cout << "Enter radius of the circle: ";
+            cin >> d;
+            cout << "Circumference of circle is " << 2 * 3.14 * d << endl;
+            break;
+        }
+        case 'S':
+        case 's': {
+            cout << "Enter the number: ";
+            cin >> d;
+            cout << "Square is " << d * d << endl;
+            break;
+        }
+        default:
+            cout << "Invalid input" << endl;
+    }
+
+    return 0;
+
+
 }
