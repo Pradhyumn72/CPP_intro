4b12bc89ba02a41692bac8cda9515a04f77bb982
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-04-06 18:03:13 +0530

    updated

---
+++ while.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//     int a=10 ;
+//     while (a>0)
+//     {
+//         cout<<a;
+//         a--;
+//     }
+//     cout<<a;
+
+//WAP to find out the how many digits in given number
+
+int a;
+cout<<"enter number "<<"\n";
+
+
+}
