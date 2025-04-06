252557b989ee6f219ef80c403f9bd10a6db3c01a
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-04-06 22:31:50 +0530

    updated

--- while.cpp
+++ while.cpp
@@ -2,18 +2,39 @@
 using namespace std;
 
 int main(){
-//     int a=10 ;
-//     while (a>0)
-//     {
-//         cout<<a;
-//         a--;
-//     }
-//     cout<<a;
+    // int a=10 ;
+    // while (a>0)
+    // {
+    //     cout<<a<<"\n";
+    //     a--;
+    // }
+    // cout<<a;
 
 //WAP to find out the how many digits in given number
 
+int a,t=0;
+cout<<"enter number";
+cin>>a;
+while (a>0)
+{
+    t=t+1;
+    a=a/10;
+}
+cout<<"total digit "<<t;
 
-int a;
-cout<<"enter number "<<"\n";
+
+
+
+// int a;
+// cout<<"enter number "<<"\n";
+// cin>>a;
+// do {
+//     if (a>0){
+//         cout<<a;
+//     }
+//     a=a/10;
+//     a++;
+
+// }while
 
 }
