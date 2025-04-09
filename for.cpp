09f2c126ceeaee77cfd25acfb83b8df124304b19
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-04-09 16:31:32 +0530

    updated

--- for.cpp
+++ for.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int x=6;
-    int y=1;
-    for(;y<=10;y++){
-        cout<<x*y<<"\n";
-    }
+    // int x=6;
+    // int y=1;
+    // for(;y<=10;y++){
+    //     cout<<x*y<<"\n";
+    // }
+
+// WAP to print even number between 1 to 10
+// WAP to print odd number between 1 to 10
+//WAP to print sum of values b/w 1 to 10
+//WAP to print multiplication of values b/w 1 to 10
+//WAP to write a program to generate a factorial
+    //WAP to display the nos from 20 to 50n which are odd
+    // int x=20;
+    // for (;x>=20 && x<=50;x++){
+    //     if (x%2 !=0){
+    //         cout<<x<<"\n";
+    //     }
+    // }
+
+//WAP to display to find out the given no is prime or not
+//WAP to find out LCM of 24 and 40 
+// WAP to find out the HCF of 24 and 40 
+//WAP to find out the factor of given number 
+int x;
+cout<<"enter no "<<endl;
+cin>>x;
+if(x%x==0 || x%1==0){
+    cout<<"prime";
+
+}
+else{
+    cout<<"not prime";
+}
+
+
 }
