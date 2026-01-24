ab8cbbaa3df71fa0d491fa065741c253a2074a65
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2026-01-24 12:54:58 +0530

    basics practice

--- test.cpp
+++ test.cpp
@@ -1,19 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int i=1;
-    int n;
-    cin>>n;
-    while(i<=n){
-        int j=1;
-        while(j<=i){
-            char ch='A'+i-1;
-
-            cout<<ch;
-            // ch++;
-            j++;
-        }
-        cout<<endl;
-        i++;
-    }
+    int i =7;
+    cout<<i++<<endl;
+    cout<<++i<<endl;
+    cout<<i--<<endl;
+    cout<<--i<<endl;
 }
